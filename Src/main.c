b7ef62e37be4e7f8bcddfd1119ed63fc362cb4c3
petrixuser <Perry.Staedtke@Student.HTW-Berlin.de>
2025-01-07 14:05:59 +0100

    add callback and to main

--- Src/main.c
+++ Src/main.c
@@ -62,6 +62,17 @@ static void MX_TIM7_Init(void);
 
 /* USER CODE END 0 */
 
+/* USER CODE BEGIN ... */
+
+/* USER CODE END ...*/
+
+
+/* USER CODE BEGIN ... */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+  HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+}
+/* USER CODE END ...*/
+
 /**
   * @brief  The application entry point.
   * @retval int
@@ -70,7 +81,7 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
-
+  
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -94,15 +105,16 @@ int main(void)
   MX_USART2_UART_Init();
   MX_TIM7_Init();
   /* USER CODE BEGIN 2 */
-
+  
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  HAL_TIM_Base_Start_IT(&htim7);
   while (1)
   {
     /* USER CODE END WHILE */
-
+    
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
